4943909c374d651965e9b57152e42b741b0ade47
s1lentj3ster <c0d3enigma@gmail.com>
2020-07-14 08:02:07 -0700

    Updated proj-2

--- proj-2.c
+++ proj-2.c
@@ -5,6 +5,9 @@
 
 void Prod();
 void Cons();
+struct semaphore Full; //Consumer
+struct semaphore Empty; //Producer
+
 
 int B,Pr,C,N;
 
@@ -14,16 +17,34 @@ int main(){
     int num[100];
     int test;
     int i = 0;
+    int k = 0;
     
-    
-    if(scanf("%d", &test) !=1){
-        num[i] = &test;
+    scanf("%d,%d,%d,%d", &B,&Pr,&C,&N);
+    while(scanf("%d", &test) != EOF){
+        printf("\n%d", test);
+        num[i] = test;
         i++;
     }
 
+
+
+    printf("\n%d\n", i);
+    /*printf("\n%d", B);
+    printf("\n%d", Pr);
+    printf("\n%d", C);
+    printf("\n%d", N);*/
+    /*while(scanf("%[^\n]%*d", &test)){
+        num[i] = test;
+        i++;
+    }
     
 
-       
+    while(k <= i){
+        printf("\n%d", num[k]);
+        k++;
+    }*/
+
+return 0;
         
     
     
